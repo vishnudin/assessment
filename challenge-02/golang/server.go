91605a6172945a055709cb16c2f56914c3668788
Robin7882 <hrobin7882@gmail.com>
2024-09-13 19:29:12 +0530

    Update server.go

--- challenge-02/golang/server.go
+++ challenge-02/golang/server.go
@@ -16,7 +16,7 @@ func main() {
 }
 
 func FileHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := os.ReadFile("file.txt")
+	data, err := os.ReadFile("file-dev.txt")
 	if err != nil {
 		panic(err)
 	}
